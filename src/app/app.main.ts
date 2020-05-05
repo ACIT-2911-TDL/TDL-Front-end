ae8aee8ba788902243d3a0227aabb58452fcf5ee
jeremyliu622 <bliu90@my.bcit.ca>
2020-05-04 23:20:28 -0700

    add today tasks function

--- src/app/app.main.ts
+++ src/app/app.main.ts
@@ -6,8 +6,11 @@ import { HttpClient } from '@angular/common/http';
     templateUrl: './app.main.html'
 })
 export class MainComponent {
+    _today;
     _allTasksArray: Array<any>;
     _sortedTasksArray: Array<any>;
+    _todayTasksArray: Array<any> = [];
+    _otherTasksArray: Array<any> = [];
     _http: HttpClient;
     selectedTask;
  
@@ -16,9 +19,23 @@ export class MainComponent {
     constructor(private http: HttpClient) {
         this._http = http;
         this.getAllTasks();
+        this._today = new Date();
+        this._today = this.formatDate(this._today);
+        console.log(this._today)
     }
 
-  
+    formatDate(_date) {
+        let month = '' + (_date.getMonth() + 1);
+        let day = '' + _date.getDate();
+        let year = _date.getFullYear();
+    
+        if (month.length < 2) 
+            month = '0' + month;
+        if (day.length < 2) 
+            day = '0' + day;
+    
+            return [year, month, day].join('-');
+    }
 
     getAllTasks() {
         let url = "http://127.0.0.1:5000/toDoTasks";
@@ -29,6 +46,14 @@ export class MainComponent {
                 this._allTasksArray[i].deadline = new Date(this._allTasksArray[i].deadline)
             }
             this._sortedTasksArray = this._allTasksArray.sort((a, b)=>  a.deadline -  b.deadline)
+            for(let i=0; i<this._sortedTasksArray.length; i++) {
+                if(this.formatDate(this._sortedTasksArray[i].deadline) == this._today) {
+                    this._todayTasksArray.push(this._sortedTasksArray[i])
+                }
+                else{
+                    this._otherTasksArray.push(this._sortedTasksArray[i])
+                }
+            }
         })
     }
 
