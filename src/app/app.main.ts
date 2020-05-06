9b8d858d258cdaf4e644c8aa7d0201f06d879305
jeremyliu622 <bliu90@my.bcit.ca>
2020-05-05 22:45:29 -0700

    update main page
    all tasks are able to be devided into time periods, still need to fix the time zone problem

--- src/app/app.main.ts
+++ src/app/app.main.ts
@@ -6,13 +6,17 @@ import { HttpClient } from '@angular/common/http';
     templateUrl: './app.main.html'
 })
 export class MainComponent {
-    _today;
     _allTasksArray: Array<any>;
     _sortedTasksArray: Array<any>;
+    _overdueTasksArray: Array<any> = [];
     _todayTasksArray: Array<any> = [];
+    _weekTasksArray: Array<any> = [];
     _otherTasksArray: Array<any> = [];
     _http: HttpClient;
-    selectedTask;
+    _today;
+    _todayTime;
+    selectedTask
+    taskDate;
  
 
 
@@ -20,21 +24,30 @@ export class MainComponent {
         this._http = http;
         this.getAllTasks();
         this._today = new Date();
+        this._todayTime = this.getTime(this._today)
         this._today = this.formatDate(this._today);
-        console.log(this._today)
+
     }
 
     formatDate(_date) {
         let month = '' + (_date.getMonth() + 1);
         let day = '' + _date.getDate();
         let year = _date.getFullYear();
+
     
         if (month.length < 2) 
             month = '0' + month;
         if (day.length < 2) 
             day = '0' + day;
     
-            return [year, month, day].join('-');
+        return year+month+day
+    }
+
+    getTime(_date) {
+        let hour = _date.getHours();
+        let min = _date.getMinutes();
+        let _time = hour*60 + min;
+        return _time
     }
 
     getAllTasks() {
@@ -42,15 +55,30 @@ export class MainComponent {
         this._http.get<any>(url)
         .subscribe(result => {
             this._allTasksArray = result;
+            
+            // order all tasks by time
             for(let i=0; i<this._allTasksArray.length; i++){
                 this._allTasksArray[i].deadline = new Date(this._allTasksArray[i].deadline)
             }
             this._sortedTasksArray = this._allTasksArray.sort((a, b)=>  a.deadline -  b.deadline)
+            console.log(this._allTasksArray)
+            // divide tasks into diiferent time periods
             for(let i=0; i<this._sortedTasksArray.length; i++) {
-                if(this.formatDate(this._sortedTasksArray[i].deadline) == this._today) {
-                    this._todayTasksArray.push(this._sortedTasksArray[i])
+                this.taskDate = this.formatDate(this._sortedTasksArray[i].deadline);
+                if(this.taskDate == this._today) {
+                    if(this.getTime(this._sortedTasksArray[i].deadline) - this._todayTime < 0) {
+                        this._overdueTasksArray.push(this._sortedTasksArray[i])
+                    }
+                    else {
+                        this._todayTasksArray.push(this._sortedTasksArray[i])
+                    }
                 }
-                else{
+                else if(this.taskDate - this._today <= 7 && this.taskDate - this._today > 0) {
+                    this._weekTasksArray.push(this._sortedTasksArray[i])
+                    console.log(this._sortedTasksArray[i].deadline)
+
+                }
+                else if(this.taskDate - this._today > 7) {
                     this._otherTasksArray.push(this._sortedTasksArray[i])
                 }
             }
@@ -68,7 +96,7 @@ export class MainComponent {
         this.http.post(url,this.selectedTask)
             .subscribe(
                 (data) => {
-                    console.log(data)
+                    // console.log(data)
                 },
                 error => {
                     alert(JSON.stringify(error));
@@ -83,7 +111,7 @@ export class MainComponent {
         this.http.post(url,this.selectedTask)
         .subscribe(
             (data) => {
-                console.log(data)
+                // console.log(data)
             },
             error => {
                 alert(JSON.stringify(error));
